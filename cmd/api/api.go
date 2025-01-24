0d62c2c24e2549ff27a66119c6843bec9ef3c77a
Alberto <albertojunior848423803@gmail.com>
2025-01-24 17:49:08 +0300

    changes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,15 @@
 package api
+
+import "database/sql"
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewApiServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
