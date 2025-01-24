f402bf835331e1002f77acb0a51a5de7584d2a36
Alberto <albertojunior848423803@gmail.com>
2025-01-24 23:39:28 +0300

    changes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,9 +2,12 @@ package api
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"github.com/alberto-debug/HttpHandler/service/user"
 )
 
 type APIServer struct {
@@ -23,6 +26,11 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subRouter := router.Path("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subRouter)
+
+	log.Println("Running at : ", s.addr)
 
 	return http.ListenAndServe(s.addr, router)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,6 @@ func main() {
 	server := api.NewAPIServer(":8080", nil)
 
 	if err := server.Run(); err != nil {
-		log.Fatal(server)
+		log.Fatal(err)
 	}
 }
