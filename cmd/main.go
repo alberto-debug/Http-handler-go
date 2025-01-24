806d3fc8ff2e56894996fe871bc091301904c992
Alberto <albertojunior848423803@gmail.com>
2025-01-24 17:46:46 +0300

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,1 @@
 package main
-
-import (
-	"log"
-
-	"github.com/alberto-debug/HttpHandler/cmd/api"
-)
-
-func main() {
-	server := api.NEWAPISERVER(":8080", nil)
-
-	if err := server.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
