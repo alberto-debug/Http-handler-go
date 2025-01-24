0d9b488e7e34f4c2e4cebb0f7915e8cd342b2f33
Alberto <albertojunior848423803@gmail.com>
2025-01-24 23:10:01 +0300

    chnages

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,35 +1,3 @@
 package api
 
-import (
-	"database/sql"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-
-	"github.com/alberto-debug/HttpHandler/service/user"
-)
-
-type APIServer struct {
-	addr string
-	db   *sql.DB
-}
-
-func NewApiServer(addr string, db *sql.DB) *APIServer {
-	return &APIServer{
-		addr: addr,
-		db:   db,
-	}
-}
-
-func (s *APIServer) Run() error {
-	router := mux.NewRouter()
-	subrouter := router.Path("/api/v1").Subrouter()
-
-	userHandler := user.NewHandler()
-	userHandler.RegisterRoutes(subrouter)
-
-	log.Println("Running at : ", s.addr)
-
-	return http.ListenAndServe(s.addr, router)
-}
+type APIServer struct{}
